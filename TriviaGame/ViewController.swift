a5116ca454736af253d1ff163fc0aa1f74ca8ed5
Caleb Ogles <calebogles@MacBook-Pro-100.local>
2018-10-18 10:04:05 -0500

    Initial Commit

---
+++ TriviaGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TriviaGame
+//
+//  Created by Caleb Ogles on 10/18/18.
+//  Copyright © 2018 Caleb Ogles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
